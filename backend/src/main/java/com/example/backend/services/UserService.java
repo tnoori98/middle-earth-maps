b71dbfc7cf82762a6faf1ac2ae439a561c533c28
tnoori98 <thomas.noori@gmail.com>
2024-01-24 23:14:45 +0100

    Pair programming with Gandalf

--- backend/src/main/java/com/example/backend/services/UserService.java
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -67,6 +67,8 @@ public class UserService {
         RegistrationResponseDto registrationResponse = new RegistrationResponseDto();
         mapper.map(dbUser, registrationResponse);
 
+        welcomeMail(registrationRequest.getEmail());
+
         return registrationResponse;
     }
 
@@ -144,6 +146,7 @@ public class UserService {
 
     public void deleteAccount(String username, String jwt) {
         User user = userRepository.getUserByUsername(username);
+        deleteAccountMail(user.getEmail());
         jwtService.blacklistJwt(jwt);
         userRepository.delete(user);
     }
@@ -169,7 +172,7 @@ public class UserService {
                             "        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n" +
                             "    </head>\n" +
                             "    <body>\n" +
-                            "        <h2>Hey, Gandalf here!</h2>\n" +
+                            "        <h2>Hey, Gandalf here! \uD83E\uDDD9\u200D♂</h2>\n" +
                             "        <p>\n" +
                             "            I've received a change request of your secret word.<br>\n" +
                             "            The one link will expire in 24 hours. If you did not request a change, chances are high Gollum did it to troll you - please ignore this email, no changes will be made to your account.<br>\n" +
@@ -178,6 +181,66 @@ public class UserService {
                             "    </body>\n" +
                             "</html>");
         } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAccountMail(String email) {
+        Optional<User> optionalUser = userRepository.findUserByEmail(email);
+        if (optionalUser.isEmpty() || !optionalUser.isPresent()) {
+            return;
+        }
+
+        try {
+            MailService.sendMail(email, "[Middle Earth Maps] Leaving the Fellowship!",
+                    "<!DOCTYPE html>\n" +
+                            "<html>\n" +
+                            "    <head>\n" +
+                            "        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">\n" +
+                            "        <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n" +
+                            "        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\n" +
+                            "        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n" +
+                            "    </head>\n" +
+                            "    <body>\n" +
+                            "        <h2>Hey!</h2>\n" +
+                            "        <p>\n" +
+                            "            Seems you've disappeared like Bilbo with the One Ring. Wishing you epic adventures wherever your path leads. Should you ever return, the Fellowship awaits!<br>\n" +
+                            "            Best, <br>\n " +
+                            "            Gandalf \uD83E\uDDD9\u200D♂ <br>\n" +
+                            "        </p>\n" +
+                            "    </body>\n" +
+                            "</html>");
+        } catch (MessagingException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void welcomeMail(String email) {
+        Optional<User> optionalUser = userRepository.findUserByEmail(email);
+        if (optionalUser.isEmpty() || !optionalUser.isPresent()) {
+            return;
+        }
+
+        try {
+            MailService.sendMail(email, "[Middle Earth Maps] Welcome to the Fellowship!",
+                    "<!DOCTYPE html>\n" +
+                            "<html>\n" +
+                            "    <head>\n" +
+                            "        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">\n" +
+                            "        <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n" +
+                            "        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\n" +
+                            "        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n" +
+                            "    </head>\n" +
+                            "    <body>\n" +
+                            "        <h2>Hey!</h2>\n" +
+                            "        <p>\n" +
+                            "            Congrats you are now part of the fellowship of the ring. Brace yourself for wild experiences on our website.<br>\n" +
+                            "            Best, <br>\n " +
+                            "            Gandalf \uD83E\uDDD9\u200D♂ <br>\n" +
+                            "        </p>\n" +
+                            "    </body>\n" +
+                            "</html>");
+        } catch (MessagingException e){
             e.printStackTrace();
         }
     }
